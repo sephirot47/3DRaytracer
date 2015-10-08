5779fafa9ac3a1f9117d52cbd777a05371613829
sephirot47 <victoriodesica777@hotmail.com>
2015-10-08 15:10:01 +0200

    OMFG ITS WORKING

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 all: main.exe
 
 main.o: main.cpp
-	g++ -std=c++11 -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system -c main.cpp -o main.o
+	g++ -O2 -std=c++11 -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system -c main.cpp -o main.o
 	
 main.exe: main.o
-	g++ main.o -std=c++11 -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system  -o main.exe
+	g++ -O2 main.o -std=c++11 -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system  -o main.exe
 
 clean:
 	rm -rf *.o *.exe
--- main.cpp
+++ main.cpp
@@ -4,32 +4,95 @@
 
 using namespace std;
 
-const int WINDOW_WIDTH = 800;
-const int WINDOW_HEIGHT = 600;
+//#define BIG
+#ifdef BIG
+const int WINDOW_WIDTH = 1600;
+const int WINDOW_HEIGHT = 1200;
+#endif
+
+#ifndef BIG
+const int WINDOW_WIDTH = 450;
+const int WINDOW_HEIGHT = 350;
+#endif
+
 const float aspectRatio = float(WINDOW_WIDTH) / WINDOW_HEIGHT;
 
 const int FOV = 60; //degrees
+const float R_FOV = FOV * 3.1415926535f/180.0f;
 const float ZNEAR = 5.0f;
+
+const float vpWidth = ZNEAR * tan(R_FOV/2);
+const float vpHeight = vpWidth / aspectRatio;
 
 float timeCount = 0.0f;
 
-bool intersectsSphere(glm::vec3 sphereCenter, float sphereRadius, glm::vec3 rayd)
+bool intersectsSphere(glm::vec3 sphereCenter, float sphereRadius, glm::vec3 rayd, 
+                      glm::vec3 &closestIntersection, glm::vec3 &normal)
 {
-    glm::vec3 rayDir = glm::normalize(rayd);
-    glm::vec3 rayOrigin = glm::vec3(.0f, .0f, .0f);
+    glm::vec3 rayOrigin = glm::vec3(0.0f);
+    glm::vec3 rayFarPoint = rayOrigin + rayd * 99999.9f;
     
-    double dot = glm::dot(rayDir, rayOrigin - sphereCenter);
-    double insideRoot = pow( dot, 2.0 ) - pow( glm::length(rayOrigin - sphereCenter), 2.0 ) + pow( sphereRadius, 2.0 );
-    return insideRoot >= 0.0;
+    float cx = sphereCenter.x;
+    float cy = sphereCenter.y;
+    float cz = sphereCenter.z;
+
+    float px = rayOrigin.x;
+    float py = rayOrigin.y;
+    float pz = rayOrigin.z;
+
+    float vx = rayFarPoint.x - px;
+    float vy = rayFarPoint.y - py;
+    float vz = rayFarPoint.z - pz;
+
+    float A = vx * vx + vy * vy + vz * vz;
+    float B = 2.0f * (px * vx + py * vy + pz * vz - vx * cx - vy * cy - vz * cz);
+    float C = px * px - 2.0f * px * cx + cx * cx + py * py - 2.0f * py * cy + cy * cy +
+                pz * pz - 2.0f * pz * cz + cz * cz - sphereRadius * sphereRadius;
+
+    // discriminant
+    float D = B * B - 4.0f * A * C;
+
+    if ( D < 0.0f )
+    {
+        closestIntersection = glm::vec3(0.0f);
+        normal = glm::normalize(closestIntersection - sphereCenter);
+        return false;
+    }
+
+    float t1 = ( -B - sqrt( D ) ) / ( 2.0f * A );
+
+    glm::vec3 solution1 = glm::vec3( rayOrigin.x * ( 1.0f - t1 ) + t1 * rayFarPoint.x,
+                                     rayOrigin.y * ( 1.0f - t1 ) + t1 * rayFarPoint.y,
+                                     rayOrigin.z * ( 1.0f - t1 ) + t1 * rayFarPoint.z );
+    
+    if ( D == 0.0f ) 
+    { 
+        closestIntersection = solution1;
+        normal = glm::normalize(closestIntersection - sphereCenter);
+        return true;
+    }
+    
+    float t2 = ( -B + sqrt( D ) ) / ( 2.0f * A );
+    glm::vec3 solution2 = glm::vec3( rayOrigin.x * ( 1.0f - t2 ) + t2 * rayFarPoint.x,
+                                     rayOrigin.y * ( 1.0f - t2 ) + t2 * rayFarPoint.y,
+                                     rayOrigin.z * ( 1.0f - t2 ) + t2 * rayFarPoint.z );
+
+    // prefer a solution that's on the line segment itself
+    if (glm::length(rayOrigin - solution1) < glm::length(rayOrigin - solution2)) 
+    {
+        closestIntersection = solution1;
+    }
+    else closestIntersection = solution2;
+    
+    
+    normal = glm::normalize(closestIntersection - sphereCenter);
+    return true;
 }
 
 glm::vec3 getRay(int pixelX, int pixelY)
 {
-    float width = ZNEAR * tan(FOV/2);
-    float height = width / aspectRatio;
-    
-    float x = -width * ( float(pixelX) / (WINDOW_WIDTH/2) );
-    float y = -height * ( float(pixelY) / (WINDOW_HEIGHT/2) );
+    float x = vpWidth * ( float(pixelX) / (WINDOW_WIDTH/2) );
+    float y = -vpHeight * ( float(pixelY) / (WINDOW_HEIGHT/2) );
     float z = ZNEAR;
     return glm::vec3(x, y, z);
 }
@@ -41,13 +104,26 @@ void drawScene(sf::RenderWindow &window, sf::Image &frameBuffer)
     {
         for(int y = -WINDOW_HEIGHT/2; y < WINDOW_HEIGHT/2; ++y)
         {
+            //float fish_factorX = cos( (x * (R_FOV / WINDOW_WIDTH) ) - (R_FOV / 2.0f ) );
+            //float fish_factorY = sin( (y  * (FOV / WINDOW_WIDTH) ) - (FOV / 2.0f) );
+            
             glm::vec3 ray = getRay(x, y);
             
             sf::Color pixelColor;
-            if(intersectsSphere(glm::vec3(sin(timeCount) * 50.0f, 0.0f, 10.0f), 5.0f, ray))
+            glm::vec3 intersection, normal;
+            
+            glm::vec3 spherePos = glm::vec3(sin(timeCount) * 2.5f, -cos(timeCount) *2.5f, sin(timeCount*3.0f) *2.0f +10.0f);
+           // glm::vec3 spherePos = glm::vec3(2.5f, 2.0f, 10.0f);
+            
+            if(intersectsSphere(spherePos, 0.5f, ray, intersection, normal))
+            {
                 pixelColor = sf::Color(255, 0, 0);
-            else
-                pixelColor = sf::Color(255, 255, 255);
+                glm::vec3 lightDir = glm::normalize(glm::vec3(1.0f, 0.0f, 1.0f));
+                float dot = glm::dot(-lightDir, normal);
+                dot = dot < 0.0f ? 0.0f : (dot > 1.0f ? 1.0f : dot);
+                pixelColor = sf::Color(pixelColor.r * dot, pixelColor.g * dot, pixelColor.b * dot);
+            }
+            else pixelColor = sf::Color(255, 255, 255);
             
             frameBuffer.setPixel(x + WINDOW_WIDTH/2, y + WINDOW_HEIGHT/2, pixelColor);
         }
